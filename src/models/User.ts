34491db0eb660cc9b9d89aa28b6a413c065d59c0
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-05 16:12:12 +0530

    feat: added support for registering events

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProps {
 	age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+    events: { [key: string]: Callback[] } = {};
+    
 	constructor(private data: UserProps) {}
 
 	get(propName: string): number | string {
@@ -13,4 +17,10 @@ export class User {
 	set(update: UserProps): void {
 		Object.assign(this.data, update);
 	}
+
+	on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
 }
