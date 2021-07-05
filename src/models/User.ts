15cdfafc68b4fdb10e2f24590efab95852f66c47
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-05 16:49:49 +0530

    feat: implemented save user

--- src/models/User.ts
+++ src/models/User.ts
@@ -28,21 +28,30 @@ export class User {
 	}
 
 	trigger(eventName: string): void {
-        const handlers = this.events[eventName];
+		const handlers = this.events[eventName];
 
-        if(!handlers || handlers.length === 0) {
-            return;
+		if (!handlers || handlers.length === 0) {
+			return;
+		}
+
+		handlers.forEach((callback) => {
+			callback();
+		});
+	}
+
+	fetch(): void {
+		axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
+			this.set(response.data);
+		});
+	}
+
+	save(): void {
+        const id = this.get('id');
+        
+		if (id) {
+            axios.put(`http://localhost:3000/users/${id}`, this.data);
+		} else {
+            axios.post(`http://localhost:3000/users`, this.data);
         }
-
-        handlers.forEach(callback => {
-            callback();
-        })
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-        .then((response: AxiosResponse): void => {
-            this.set(response.data);
-        });
-    }
+	}
 }
