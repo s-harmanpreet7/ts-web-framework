79d632eac68feeec5307eb260c1ae04182465bf4
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-06 18:09:21 +0530

    refactor: extracted abstract View from UserForm

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,20 +1,12 @@
 import { User } from '../models/User';
+import { View } from './View';
 
-export class UserForm {
-	constructor(public parent: Element, public model: User) {
-		this.bindModel();
-	}
-
-	bindModel(): void {
-		this.model.on('change', () => {
-			this.render();
-		});;;
-	}
+export class UserForm extends View {
 
 	eventsMap(): { [key: string]: () => void } {
 		return {
 			'click:.set-age': this.onSetAgeClick,
-			'click:.set-name': this.onSetNameClick
+			'click:.set-name': this.onSetNameClick,
 		};
 	}
 
@@ -33,7 +25,7 @@ export class UserForm {
 
 	onSetAgeClick = (): void => {
 		this.model.setRandomAge();
-	}
+	};
 
 	onSetNameClick = (): void => {
 		const input = this.parent.querySelector('input');
@@ -42,27 +34,5 @@ export class UserForm {
 			const name = input.value;
 			this.model.set({ name });
 		}
-	}
-
-	bindEvents(fragment: DocumentFragment): void {
-		const eventsMap = this.eventsMap();
-
-		for (const eventKey in eventsMap) {
-			const [eventName, selector] = eventKey.split(':');
-
-			fragment.querySelectorAll(selector).forEach((element) => {
-				element.addEventListener(eventName, eventsMap[eventKey]);
-			});
-		}
-	}
-
-	render(): void {
-		this.parent.innerHTML = '';
-
-		const templateElement = document.createElement('template');
-		templateElement.innerHTML = this.template();
-
-		this.bindEvents(templateElement.content);
-		this.parent.append(templateElement.content);
-	}
+	};
 }
