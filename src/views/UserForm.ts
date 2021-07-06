80fd8aa888fdef331f0f5781fdc16786f47822b1
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-07-06 18:30:28 +0530

    feat: Save user from UI

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,23 +3,21 @@ import { User } from '../models/User';
 import { View } from './View';
 
 export class UserForm extends View<User, UserProps> {
-
 	eventsMap(): { [key: string]: () => void } {
 		return {
 			'click:.set-age': this.onSetAgeClick,
 			'click:.set-name': this.onSetNameClick,
+			'click:.save-user': this.onSaveClick,
 		};
 	}
 
 	template(): string {
 		return `
             <div>
-                <h1>User Form</h1>
-                <div>Name   : ${this.model.get('name')}</div>
-                <div>Age    : ${this.model.get('age')}</div>
-                <input />
+                <input placeholder="${this.model.get('name')}"/>
                 <button class="set-name">Change Name</button>
                 <button class="set-age">Set random age</button>
+				<button class="save-user">Save</button>
             </div>
         `;
 	}
@@ -35,5 +33,9 @@ export class UserForm extends View<User, UserProps> {
 			const name = input.value;
 			this.model.set({ name });
 		}
+	};
+
+	onSaveClick = (): void => {
+		this.model.save();
 	};
 }
